93bb997771300b9fc3a71977dffe2aef41b2e3db
Dan Christian <92633841+0DanChristian@users.noreply.github.com>
2021-10-31 14:58:06 +0800

    AppleOrange.py

---
+++ AppleOrange.py
@@ -0,0 +1,17 @@
+
+def ppl_qstn():
+    _apple = int(input("Our apple costs 20 pesos each, how many apples do you want to buy? "))
+    return _apple
+
+def rng_qstn():
+    _orange = int(input("Our orange costs 25 pesos each, how many oranges do you want to buy? "))
+    return _orange
+
+
+
+ppl_qstn()
+
+rng_qstn()
+
+
+
