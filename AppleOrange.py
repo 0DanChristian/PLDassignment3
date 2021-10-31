e0f802696db8ac544c42bec0483b15d03263e46a
Dan Christian <92633841+0DanChristian@users.noreply.github.com>
2021-10-31 22:35:06 +0800

    AppleOrange.py

--- AppleOrange.py
+++ AppleOrange.py
@@ -10,4 +10,12 @@ def bOrange():
     _rngmmnt = int(input("How many orange do you want to buy? "))
     return _rngmmnt
 
+def ppl_prc():
+    _pplprc = 20
+    print(f"Our apple costs {_pplprc} pesos each. ")
+    return _pplprc
 
+def rng_prc():
+    _rngprc = 25
+    print(f"Our orange costs {_rngprc} pesos each. ")
+    return _rngprc
