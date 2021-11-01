a956185e28f5c6ebccb17c619aae2c9a993e3dc2
Dan Christian <92633841+0DanChristian@users.noreply.github.com>
2021-11-01 21:20:51 +0800

    Money.py

--- Money.py
+++ Money.py
@@ -4,3 +4,16 @@
 # Format: You can buy ___ apples and your change is ____ pesos.
 
 
+def aMoney():
+    _money = int(input("How much money do you have? "))
+    return _money
+
+
+
+# steps
+# 1. ask for the amount of money you have then save as variable
+amountMoney = aMoney()
+# 2. ask for the price of an apple then save as variable
+# 3. solve for the total amount of apples you can buy then save as variable 
+# 4. solve for the change then save as variable
+# 5. display the total number of apples and the change
